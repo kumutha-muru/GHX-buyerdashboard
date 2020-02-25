4fff12bf2cddb5cd3b5c31b8ba746112688a86d1
Ramesh Rajendran <ramrajendran@ghx.com>
2020-02-25 19:29:28 +0530

    25 last updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,12 +10,15 @@ import {MatTableDataSource} from '@angular/material/table';
 export class HomeComponent implements OnInit {
 
   displayedColumns: string[] = [ 'name', 'company', 'signintime'];
+  displayedColumns2: string[] = [ 'date','time','rep', 'company', 'location','department','subject'];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  dataSource2= new MatTableDataSource<PeriodicElement1>(ELEMENT_DATA1);
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   ngOnInit() {
     this.dataSource.paginator = this.paginator;
+    this.dataSource2.paginator = this.paginator;
   }
 }
 
@@ -23,6 +26,15 @@ export interface PeriodicElement {
   name: string;
   company: string;
   signintime: number|string;
+}
+export interface PeriodicElement1{
+  date:number|string;
+  time:number|string;
+  rep:string;
+  company: string;
+  location:string;
+  department:string;
+  subject:string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
@@ -34,5 +46,8 @@ const ELEMENT_DATA: PeriodicElement[] = [
   { name: 'Ryan Mason', signintime:  'Jun 16, 2016 06:54 PM', company: 'Styker'},
 ];
 
-
+const ELEMENT_DATA1: PeriodicElement1[] = [
+  {date:'Mar 13, 2019',time:'08:00 AM',rep:'reponeupdated,carolineupdated',company:'Tested_02Oct_Company 1',location:'chennai',department:'Cath Lab',subject:'OTA One Year'},
+  {date:'May 17, 2018',time:'12:00 PM',rep:'fname lname',company:'Tested_28Sep_Company 1',location:'Florida',department:'Cath Lab',subject:'Long Appt'}
+]
 
